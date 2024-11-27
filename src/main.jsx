96a2c6bcf941f2256c636b6d97ad689fafc843d3
innasnegurova <innasnegurova@nxte.nl>
2024-11-27 21:21:26 +0100

    init: set project structure and packages

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import 'modern-normalize';
 import './index.css';
-import App from './components/App.jsx';
+import App from './components/App/App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
